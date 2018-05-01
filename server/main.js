22a7806f0599e119e505a8eaa94c4b9e0e8668b5
suryamurugan <surya3542live@gmail.com>
2018-05-01 18:14:24 +0530

    browserify and reactiy and .. scaffolding the app

--- server/main.js
+++ server/main.js
@@ -2,10 +2,9 @@ var express = require('express');
 
 var app = new express();
 
+
 app.get('/',function(req,res){
 
 res.render('./../app/index.ejs')
 
-}).listen(7777);
-
-
+}).use(express.static(__dirname + '/../.tmp')).listen(7777);
