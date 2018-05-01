e325d5434a4ad4de5eac305078b10e745a68547f
suryamurugan <surya3542live@gmail.com>
2018-05-01 18:48:31 +0530

    working git add . creasting react component

--- app/main.jsx
+++ app/main.jsx
@@ -1,1 +1,9 @@
-console.log("YAYA");
+var ReactDOM = require('react-dom');
+var React = require('react');
+
+
+console.log("Hello from JSX!");
+
+var GroceryItemList = require('./components/GroceryItemList.jsx');
+
+ReactDOM.render(<GroceryItemList />,app)
--- server/main.js
+++ server/main.js
@@ -2,9 +2,8 @@ var express = require('express');
 
 var app = new express();
 
-
 app.get('/',function(req,res){
-
-res.render('./../app/index.ejs')
-
-}).use(express.static(__dirname + '/../.tmp')).listen(7777);
+    res.render('./../app/index.ejs',{});
+})
+.use(express.static(__dirname + '/../.tmp'))
+.listen(7777);
